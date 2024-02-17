cb337e64e6d184338eea1a4a45a866d17f666c9c
Krisha <krishapatel1613@gmail.com>
2024-02-18 00:00:12 +0530

    Added script file

---
+++ 24_script_tag/script.js
@@ -0,0 +1,14 @@
+/* console.log("Hello world");
+
+ var a= [1,2,3,4,5];
+var b= [...a];
+b.pop();
+console.log(b);
+console.log(a);  */
+
+console.log("Hey");
+setTimeout(function(){
+            console.log("Hey 2");
+}, 3000)
+
+console.log("Hey 3");
